4cfbe9527904eac336d412c9838872bb9fc45534
kevin Chen <willietanada@gmail.com>
2018-08-28 16:37:30 +0800

    change param (fc1weight...)

--- src/neuralTest/connect.java
+++ src/neuralTest/connect.java
@@ -1,26 +1,141 @@
 package neuralTest;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.lang.Object;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+
+import org.openimaj.audio.AudioFormat;
+import org.openimaj.audio.AudioPlayer;
+import org.openimaj.audio.JavaSoundAudioGrabber;
+import org.openimaj.audio.SampleChunk;
+import org.openimaj.audio.analysis.FourierTransform;
+import org.openimaj.audio.features.MFCC;
+import org.openimaj.audio.processor.FixedSizeSampleAudioProcessor;
+import org.openimaj.video.xuggle.XuggleAudio;
+import org.openimaj.vis.audio.AudioWaveform;
+
+import edu.cmu.sphinx.frontend.Data;
+import edu.cmu.sphinx.frontend.DataProcessor;
+import edu.cmu.sphinx.frontend.FrontEnd;
+import edu.cmu.sphinx.frontend.frequencywarp.MelFrequencyFilterBank;
+import edu.cmu.sphinx.frontend.transform.DiscreteCosineTransform2;
+import edu.cmu.sphinx.frontend.transform.DiscreteFourierTransform;
+import edu.cmu.sphinx.frontend.util.AudioFileDataSource;
+
 public class connect {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws MalformedURLException {
+		//		XuggleAudio xa = new XuggleAudio( new File( "myAudioFile.mp3" ) );
+		//		AudioPlayer.createAudioPlayer( xa ).run();
+		//		
+		//		final AudioWaveform vis = new AudioWaveform( 400, 400 );
+		//		vis.showWindow( "Waveform" );
+		//
+		//		final XuggleAudio xa = new XuggleAudio( 
+		//		    new URL( "http://www.audiocheck.net/download.php?" +
+		//		        "filename=Audio/audiocheck.net_sweep20-20klin.wav" ) );
+		//
+		//		SampleChunk sc = null;
+		//		while( (sc = xa.nextSampleChunk()) != null )
+		//		    vis.setData( sc.getSampleBuffer() );
+		//		
+		//		
+		//		MFCC mfcc = new MFCC( xa );
+		//		
+		//		while( (sc = mfcc.nextSampleChunk()) != null )
+		//		{
+		//			double[][] mfccs = mfcc.getLastCalculatedFeature();
+		//			vis.setData( mfccs[0] );
+		//		}
+		//		
+		//		JavaSoundAudioGrabber jsag = new JavaSoundAudioGrabber( new AudioFormat( 16, 44.1, 1 ) );
+		//		FixedSizeSampleAudioProcessor fssap = new FixedSizeSampleAudioProcessor( jsag, 441*3, 441 );
+
+		AudioFileDataSource audioDataSource = new AudioFileDataSource(3200, null);
+
+		audioDataSource.setAudioFile(new URL("file:///path/to/my.wav", "source");
+
+		final ArrayList<DataProcessor> pipeline = new ArrayList<DataProcessor>();
+
+		pipeline.add(audioSource);
+		pipeline.add(new DiscreteFourierTransform());
+		pipeline.add(new MelFrequencyFilterBank(minFreq, maxFreq, numFilters));
+		pipeline.add(new DiscreteCosineTransform2(numFilters, 12));
+		FrontEnd f = new FrontEnd(pipeline);
+
+		Data mfccs;
+		do {
+			mfccs = f.getData();
+		} 
+		while(mfccs != null) 
+
 
 
 	}
 
 
 	public static int MLCal (double[] input) {	
-		
-		//parameters
-		double [][] fc1Weight = {{0.071791708,-0.273520201,0.209405884,0.195195079, 			-1.121803045	, 			-0.996459961	, 			0.594318748	, 			-0.309516639	, 			-1.338285089	, 			-1.883283019	, 			-1.113033533	, 			-0.1713797	, 			-0.01577769	, 			-0.014156765	, 			-0.002883073	, 			-0.001924099	, 			0.000884003	, 			-0.002898041	, 			0.010487561	, 			0.001051584	, 			0.000437031	, 			0.00332519	, 			-0.005168776	, 			0.001920618	, 			0.014992776	, 			0.001920314	, 			-0.012455763	, 			-0.015181897	, 			-0.015036445	, 			0.003294797	, 			-0.000655362	, 			-0.007562938	, 			-0.000557545	, 			-0.003645791	, 			-0.002838991	, 			-0.002225966	},
-				{0.322245032	, 				0.542218029	, 				-1.475345016	, 				-0.884903848	, 				0.882239282	, 				-1.088714719	, 				-0.16587843	, 				-0.512780726	, 				-0.632877171	, 				-0.725718021	, 				-0.158854783	, 				-0.056672558	, 				-0.027033886	, 				-0.008661306	, 				-0.017975984	, 				0.003737505	, 				-0.00704545	, 				0.007667117	, 				0.006916017	, 				0.006850094	, 				-0.011783908	, 				0.004049529	, 				-0.001875732	, 				0.003250984	, 				0.014310873	, 				-0.001892314	, 				-0.001696224	, 				-0.002976529	, 				-0.01085856	, 				-0.000336136	, 				-0.001807211	, 				-0.009280751	, 				0.004591503	, 				0.004324209	, 				0.006530917	, 				-0.002072874	},
-				{-0.119724207	, 					0.975386143	, 					-0.145098701	, 					-0.605136037	, 					0.493678272	, 					-0.637068391	, 					-0.739637494	, 					0.209786251	, 					-0.51344043	, 					-0.846280217	, 					-0.132942826	, 					-0.009992044	, 					-0.01234982	, 					-0.007055669	, 					-0.002471291	, 					0.003669044	, 					0.011077397	, 					-0.008513085	, 					-0.00282329	, 					0.006509914	, 					-0.00314883	, 					-0.007842547	, 					-0.007368789	, 					0.000508582	, 					-0.00902913	, 					-0.008432669	, 					0.003158953	, 					0.008495963	, 					-0.006984553	, 					0.008239323	, 					-0.003509283	, 					-0.004746191	, 					-0.000543773	, 					0.001631273	, 					-0.008747756	, 					0.001240238	}};
 
-		
-		double[][] fc2Weight = {{1.558101892	,	1.135969877	,	1.699363351	},
-				{-1.556752801,-1.135006785,-1.704803467}};
-		
-		double [] fc1Bias = {0.007558372	,-0.065355577	,	-0.024339819	};
-		double [] fc2Bias = {-1.071723938,1.071723938};
+		//parameters
+		double [][] fc1Weight = {
+				{	-0.233562514
+					,	0.073163345
+					,	0.162523463
+					,	-0.475291908
+					,	0.153847501
+					,	0.250875354
+					,	-0.159885272
+					,	0.325949907
+					,	0.007859653
+					,	0.097039938
+					,	-0.161264479
+					,	-0.205180123},
+
+				{	-0.30799529
+						,	0.031105876
+						,	0.127346873
+						,	-0.564684391
+						,	0.077051081
+						,	0.169108436
+						,	-0.327682018
+						,	0.358338505
+						,	-0.019924065
+						,	0.106168672
+						,	-0.206807643
+						,	-0.206969202},
+
+				{	-0.329386055
+							,	0.097163714
+							,	0.127564043
+							,	-0.512097239
+							,	0.154471964
+							,	0.191515937
+							,	-0.258915633
+							,	0.2857714
+							,	-0.052398041
+							,	0.153321788
+							,	-0.155209497
+							,	-0.183188558}
+
+
+		};
+		double[][] fc2Weight = {
+				{0.368401468
+					,0.411503017
+					,0.390290767},
+				{-0.358836472
+						,	-0.406123072
+						,	-0.384561062}
+		};
+
+		double [] fc1Bias = {-0.013109485
+				,-0.006122951
+				,0.001943798};
+		double [] fc2Bias = {0.196922183,-0.196922451};
 		//declare first layer output
 		double[] fc1Output = {0,0,0};
 		//first layer calculation
@@ -39,10 +154,10 @@ public class connect {
 			}	
 			fc2Output[j] = fc2Output[j] +fc2Bias[j] ;
 		}
-		
+
 		//throw in second layer output and get softmax output
 		double [] output  = softmax(fc2Output);
-		
+
 		//if the first one has larger probability return 0, else return 1
 		if(output[0]>output[1]) {
 			return 0;
@@ -51,9 +166,9 @@ public class connect {
 			return 1;
 		}
 	}
-	
-	
-	
+
+
+
 	public static double[] softmax (double [] input) {
 		//declare output
 		double [] output = new double [input.length];
